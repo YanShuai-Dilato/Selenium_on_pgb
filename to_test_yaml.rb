96693e44275cd132553f72ee04aa46d3b20363c4
YanShuai-Dilato <shuai.yan@dilatoit.com>
2013-03-27 11:13:40 +0800

    update README

--- to_test_yaml.rb
+++
@@ -1,9 +0,0 @@
-# Just to verify whether some file(s) work OK or not
-
-require 'yaml'
-
-yml = YAML::load(File.read("data/data_xpath.yaml"))
-url = YAML::load(File.read("data/data_url.yaml"))
-
-puts yml[:sign_in_page][:i_forgot_my_password]
-puts url[:forgot_your_password]
