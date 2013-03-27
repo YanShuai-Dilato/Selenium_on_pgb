b5be0ec6caa371f33b817156bd5bae04ef342182
YanShuai-Dilato <shuai.yan@dilatoit.com>
2013-03-27 16:15:41 +0800

    Update new_app case using AppDetailVerify module

---
+++ lib/app_detail_verify.rb
@@ -0,0 +1,23 @@
+# helper method for verify the new-created app's details
+# the details include: 
+#     title
+#     description
+#     version
+#     source
+
+module AppDetailVerify 
+  
+  # verify whether the app's title equals to the designated string. 
+  def title_of_app_by_zip 
+    # Hello Worldéèàù
+  end
+  
+  # verify whether the app's source equals to the app's github repo address. 
+  def source_of_app_by_repo
+    @actual_text = @driver.find_element(:xpath => @data_xpath[:new_app_detail][:source]).text + "\n"
+  end
+  
+  #
+  # will add more test cases to verify the other properties.
+  #
+end
--- testcases/new_app_rspec.rb
+++ testcases/new_app_rspec.rb
@@ -8,16 +8,19 @@ require_relative "../action/sign_in_page"
 require_relative "../action/new_app_page"
 require_relative "../tools/new_app_dialog"
 require_relative "../data/base_env"
+require_relative "../lib/app_detail_verify"
 
 describe "Create an app " do
     include NewAppDialog
     include BaseEnv
+    include AppDetailVerify
 
     before(:all) do
         @driver = browser
         @url = base_url + "/people/sign_in"
         @driver.get @url
         @data_xpath = YAML::load(File.read("../data/data_xpath.yml"))
+        @app_data = YAML::load(File.read("../data/data_app.yml"))
         @data_user = YAML::load(File.read(File.expand_path("../../data/data_user.yml",__FILE__)))
         SignInPage.new(@driver).sign_in_with_adobe_id(@data_user[:en_us][:free_adobeid][:id],@data_user[:en_us][:free_adobeid][:password])
         sleep 5
@@ -25,18 +28,18 @@ describe "Create an app " do
     end
 
     before(:each) do
-        @click_element = NewAppPage.new(@driver)
+        @new_app_page = NewAppPage.new(@driver)
     end
 
     after(:each) do
-        @click_element.close_current_browser
+        @new_app_page.close_current_browser
     end
-
+=begin
     describe "-> by zip file " do 
         it "should work successfully" do
             @num_article = @driver.find_elements(:tag_name => "article").count
             # puts "Original num of article was " + @num_article.to_s
-            @click_element.new_app_with_zip
+            @new_app_page.new_app_with_zip
             wait_for_element_present(500,:css,"app-summary ui-block new")
             @num_article_after = @driver.find_elements(:tag_name => "article").count
             puts "Changed num of article was " + @num_article_after.to_s
@@ -49,16 +52,13 @@ describe "Create an app " do
             #end
         end
     end
-=begin
+=end
     # Pass
     describe "-> by git repo " do 
         it "should work successfully" do 
-            #@driver = browser
-            @num_article = @driver.find_elements(:tag_name => "article").count
-            @click_element.new_app_with_pub_repo
-            @num_article_after = @driver.find_elements(:tag_name => "article").count
-            @num_article_after.should == @num_article + 1
+            @new_app_page.new_app_with_pub_repo
+            source_of_app_by_repo.should == @app_data[:new_app][:by_repo]
         end
     end
-=end
+
 end
