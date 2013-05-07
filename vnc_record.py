b7ec11dbe7460407834b49ab87ce6873c3e3c32f
LichuanLu <lulichuan628@gmail.com>
2013-05-07 14:28:25 +0800

    update vnc

--- vnc_record.py
+++ vnc_record.py
@@ -1,16 +1,21 @@
 from castro import Castro
 import subprocess,os,sys
 from subprocess import Popen, PIPE, STDOUT
-if __name__ == '__main__':	
+if sys.platform == "darwin":
 	print 'launching slave process...'
 	env = os.environ.copy()
 	print env
-	if sys.platform == "Darwin":
-		c=Castro(display=1,passwd="/Users/labuser/.vinevncauth")
-		c.start()
-		ruby=subprocess.call(['rake'],env=env)
-		c.stop()
-	else:
+	print sys.platform
+	c=Castro(display=1,passwd="/Users/labuser/.vinevncauth")
+	c.start()
+	ruby=subprocess.call(['rake'],env=env)
+	c.stop()
+else:
+	if __name__ == '__main__':
+		print 'launching slave process...'
+		env = os.environ.copy()
+		print env
+		print sys.platform
 		c=Castro(display=1)
 		c=start()
 		ruby=subprocess.call(['ruby','-S','rake'],env=env)
