0e885a8c36766b7a8ed4b4b45d50231cac24d7ea
LichuanLu <lulichuan628@gmail.com>
2013-05-07 12:47:25 +0800

    update vnc

--- vnc_record.py
+++ vnc_record.py
@@ -3,6 +3,7 @@ import subprocess,os
 from subprocess import Popen, PIPE, STDOUT
 print 'launching slave process...'
 env = os.environ.copy()
+print env
 c=Castro(display=1,passwd="/Users/labuser/.vinevncauth")
 c.start()
 ruby = subprocess.call(['ruby','rake','TC_001'],env=env)
