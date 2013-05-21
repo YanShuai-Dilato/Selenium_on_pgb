7426035bffb799e8c78d1afe8a0be771516e0547
LichuanLu <lulichuan628@gmail.com>
2013-05-21 16:26:25 +0800

    update vnc

--- vnc_record.py
+++ vnc_record.py
@@ -11,8 +11,10 @@ if sys.platform == "darwin":
 		tc_name = "TC_%03d" % x # TC_001 TC_002 ... TC_008
 		c=Castro(display=1, passwd="/Users/labuser/.vinevncauth", filename= tc_name + "_screencast.swf")
 		c.start()
+		print "ready to start"
 		ruby = subprocess.Popen(['rake',tc_name],env=env)
 		try:
+			print "ready to wait"
 			ruby.wait(timeout=3600)
 		except:
 			ruby.kill()
